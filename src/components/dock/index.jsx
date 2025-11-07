b4baaad8f136a2dfc4c1face680f8f2dfe0a3997
Roannovna <RoAnna.97line@gmail.com>
2025-11-07 12:29:00 +0300

    feat: added a new site to the dock

--- src/components/dock/index.jsx
+++ src/components/dock/index.jsx
@@ -8,7 +8,7 @@ export function Dock() {
   return (
     <div className={style.dock__container}>
       <button className={style.dock__button} onClick={() => setShowFrame(true)}>
-        <img src={projectIcons.slozhnoSosredotochitsya} alt="slozhno-sosredotochitsya" />
+        <img src={projectIcons.slozhnoSosredotochitsya} alt="иконка сайта сложно сосредоточиться" />
       </button>
       {showFrame && (
         <div className={style.dock__frame}>
@@ -16,6 +16,18 @@ export function Dock() {
           <iframe
             src='https://roannovna.github.io/slozhno-sosredotochitsya/' 
             title="Сложно сосредоточиться"
+          />
+        </div>
+      )}
+      <button className={style.dock__button} onClick={() => setShowFrame(true)}>
+        <img src={projectIcons.zakrivayuschiyTeg} alt="иконка сайта закрывающий тег" />
+      </button>
+      {showFrame && (
+        <div className={style.dock__frame}>
+          <button className={style.dock__close} onClick={() => setShowFrame(false)}></button>
+          <iframe
+            src='https://roannovna.github.io/zakrivayuschiy-teg/' 
+            title="Закрывающий тег"
           />
         </div>
       )}
