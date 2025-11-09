f5c593f0479fef23508f47d4491fa1f13c62035e
Roannovna <RoAnna.97line@gmail.com>
2025-11-09 20:20:21 +0300

    feat: added links for tech stack icons & implemented display in iframe

--- src/components/tech-stack/index.jsx
+++ src/components/tech-stack/index.jsx
@@ -1,17 +1,37 @@
+import style from "./tech-stack.module.css";
 import { techStackIcons } from "./icons/imp";
 import { ipadTokens } from '/src/tokens/ipad-ui-tokens';
+import { Iframe } from '../UI/iframe';
+import { useState } from 'react';
 
-const iconSize = { width: ipadTokens.icon.app.w, height: ipadTokens.icon.app.w };
+
+const iconSize = { width: ipadTokens.icon.app.w, height: ipadTokens.icon.app.w, borderRadius: ipadTokens.icon.app.br };
 
 export function TechStack({ className }) {
+  const [frameSrc, setFrameSrc] = useState(null);
+  
   return (
     <>
-      <img className={className} src={techStackIcons.tsIcon} alt="TypeScript" style={{...iconSize, display: 'block'}} />
-      <img className={className} src={techStackIcons.jsIcon} alt="JavaScript" style={{...iconSize, display: 'block'}} />
-      <img className={className} src={techStackIcons.htmlIcon} alt="HTML" style={{...iconSize, display: 'block'}} />
-      <img className={className} src={techStackIcons.cssIcon} alt="CSS" style={{...iconSize, display: 'block'}} />
-      <img className={className} src={techStackIcons.reactIcon} alt="React" style={{...iconSize, display: 'block'}} />
-      <img className={className} src={techStackIcons.muiIcon} alt="Material UI" style={{...iconSize, display: 'block'}} />
+      <button style={iconSize} onClick={() => setFrameSrc('https://www.typescriptlang.org/')}>
+        <img className={className} src={techStackIcons.tsIcon} alt="TypeScript" style={iconSize} />
+      </button>
+      <button style={iconSize} onClick={() => setFrameSrc('https://doka.guide/js')} >
+        <img className={className} src={techStackIcons.jsIcon} alt="JavaScript" style={iconSize} />
+      </button>
+      <button style={iconSize} onClick={() => setFrameSrc('https://doka.guide/html')} >
+        <img className={className} src={techStackIcons.htmlIcon} alt="HTML" style={iconSize} />
+      </button>
+      <button style={iconSize} onClick={() => setFrameSrc('https://doka.guide/css')} >
+        <img className={className} src={techStackIcons.cssIcon} alt="CSS" style={iconSize} />
+      </button>
+      <button style={iconSize} onClick={() => setFrameSrc('https://react.dev/')} >
+        <img className={className} src={techStackIcons.reactIcon} alt="React" style={iconSize} />
+      </button>
+      <button style={iconSize} onClick={() => setFrameSrc('https://mui.com/')} >
+        <img className={className} src={techStackIcons.muiIcon} alt="Material UI" style={iconSize} />
+      </button>
+
+      <Iframe open={!!frameSrc} src={frameSrc} onClose={() => setFrameSrc(null)} />
     </>
   )
 }
