a37db72aad70845cca79cadad8d8122f84e648f8
Roannovna <RoAnna.97line@gmail.com>
2025-11-10 11:42:42 +0300

    fix: button size

--- src/components/tech-stack/index.jsx
+++ src/components/tech-stack/index.jsx
@@ -1,35 +1,22 @@
-import style from "./tech-stack.module.css";
 import { techStackIcons } from "./icons/imp";
 import { ipadTokens } from '/src/tokens/ipad-ui-tokens';
 import { Iframe } from '../UI/iframe';
 import { useState } from 'react';
 
 
-const iconSize = { width: ipadTokens.icon.app.w, height: ipadTokens.icon.app.w, borderRadius: ipadTokens.icon.app.br };
+const iconSize = { width: ipadTokens.icon.app.w, height: ipadTokens.icon.app.w };
 
 export function TechStack({ className }) {
   const [frameSrc, setFrameSrc] = useState(null);
   
   return (
     <>
-      <button style={iconSize} onClick={() => setFrameSrc('https://www.typescriptlang.org/')}>
-        <img className={className} src={techStackIcons.tsIcon} alt="TypeScript" style={iconSize} />
-      </button>
-      <button style={iconSize} onClick={() => setFrameSrc('https://doka.guide/js')} >
-        <img className={className} src={techStackIcons.jsIcon} alt="JavaScript" style={iconSize} />
-      </button>
-      <button style={iconSize} onClick={() => setFrameSrc('https://doka.guide/html')} >
-        <img className={className} src={techStackIcons.htmlIcon} alt="HTML" style={iconSize} />
-      </button>
-      <button style={iconSize} onClick={() => setFrameSrc('https://doka.guide/css')} >
-        <img className={className} src={techStackIcons.cssIcon} alt="CSS" style={iconSize} />
-      </button>
-      <button style={iconSize} onClick={() => setFrameSrc('https://react.dev/')} >
-        <img className={className} src={techStackIcons.reactIcon} alt="React" style={iconSize} />
-      </button>
-      <button style={iconSize} onClick={() => setFrameSrc('https://mui.com/')} >
-        <img className={className} src={techStackIcons.muiIcon} alt="Material UI" style={iconSize} />
-      </button>
+      <img className={className} src={techStackIcons.tsIcon} onClick={() => setFrameSrc('https://www.typescriptlang.org/')} alt="TypeScript" style={iconSize} />
+      <img className={className} src={techStackIcons.jsIcon} onClick={() => setFrameSrc('https://doka.guide/js')} alt="JavaScript" style={iconSize} />
+      <img className={className} src={techStackIcons.htmlIcon} onClick={() => setFrameSrc('https://doka.guide/html')} alt="HTML" style={iconSize} />
+      <img className={className} src={techStackIcons.cssIcon} onClick={() => setFrameSrc('https://doka.guide/css')} alt="CSS" style={iconSize} />
+      <img className={className} src={techStackIcons.reactIcon} onClick={() => setFrameSrc('https://react.dev/')} alt="React" style={iconSize} />
+      <img className={className} src={techStackIcons.muiIcon} onClick={() => setFrameSrc('https://mui.com/')} alt="Material UI" style={iconSize} />
 
       <Iframe open={!!frameSrc} src={frameSrc} onClose={() => setFrameSrc(null)} />
     </>
