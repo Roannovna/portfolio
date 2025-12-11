169658e4b15d19259f011490ddaaed2f406bfc79
Roannovna <RoAnna.97line@gmail.com>
2025-12-11 19:10:09 +0300

    feat: added buttons for navigation

--- src/assets/project-icons/imp.js
+++ src/assets/project-icons/imp.js
@@ -1,4 +1,6 @@
+import contactsIcon from "./contacts-icon.svg";
+import homeIcon from "./home-icon.svg";
 import slozhnoSosredotochitsya from "./slozhno-sosredotochitsya.svg";
 import zakrivayuschiyTeg from "./zakrivayuschiy-teg.svg";
 
-export const projectIcons = { slozhnoSosredotochitsya, zakrivayuschiyTeg };
+export const projectIcons = { slozhnoSosredotochitsya, zakrivayuschiyTeg, contactsIcon, homeIcon };
--- src/components/dock/index.jsx
+++ src/components/dock/index.jsx
@@ -1,27 +1,27 @@
-import style from './dock.module.css'
 import { projectIcons } from '@/assets/project-icons/imp';
-import { useState } from 'react';
-import { ipadTokens } from '@/tokens/ipad-ui-tokens.jsx';
-import { Iframe } from '@/components/UI/iframe';
 import { tourCastomIcons } from '@/assets/tour/imp';
+import { Iframe } from '@/components/UI/iframe';
 import { CustomBtn } from '@/components/UI/tour/custom-btn.js';
+import { ipadTokens } from '@/tokens/ipad-ui-tokens.jsx';
+import { useState } from 'react';
+import { useNavigate } from 'react-router';
+import style from './dock.module.css';
 
-const iconSize = { 
-  width: ipadTokens.icon.spot.w, 
-  height: ipadTokens.icon.spot.h, 
-  borderRadius: ipadTokens.icon.spot.br 
+const iconSize = {
+  width: ipadTokens.icon.spot.w,
+  height: ipadTokens.icon.spot.h,
+  borderRadius: ipadTokens.icon.spot.br
 };
 
 export function Dock({ id, className, style: extraStyle, onOpenTour }) {
   const [frameSrc, setFrameSrc] = useState(null);
+  const navigate = useNavigate();
 
   return (
     <>
-      <footer  id={id} className={`${style.dock__container} ${className}`} style={{...extraStyle}}>
+      <footer id={id} className={`${style.dock__container} ${className}`} style={{ ...extraStyle }}>
 
-        <CustomBtn type="primary" onClick={onOpenTour}>
-          <img src={tourCastomIcons.tourBtn} style={iconSize} alt="иконка тура" />
-        </CustomBtn>
+        <button className={style.dock__button} style={{ ...iconSize, backgroundImage: `url(${projectIcons.homeIcon})`, backgroundSize: 'contain', backgroundPosition: 'center' }} onClick={() => navigate('/')}></button>
 
         <button style={iconSize} className={style.dock__button} onClick={() => setFrameSrc('https://roannovna.github.io/slozhno-sosredotochitsya/')}>
           <img src={projectIcons.slozhnoSosredotochitsya} alt="иконка сайта сложно сосредоточиться" />
@@ -30,6 +30,14 @@ export function Dock({ id, className, style: extraStyle, onOpenTour }) {
         <button style={iconSize} className={style.dock__button} onClick={() => setFrameSrc('https://roannovna.github.io/zakrivayuschiy-teg/')}>
           <img src={projectIcons.zakrivayuschiyTeg} alt="иконка сайта закрывающий тег" />
         </button>
+
+        <button className={style.dock__button} style={{ ...iconSize, backgroundImage: `url(${projectIcons.contactsIcon})`, backgroundSize: 'contain', backgroundPosition: 'center' }} onClick={() => navigate('/contacts')}></button>
+
+        <div className={style.dock__separator}></div>
+
+        <CustomBtn type="primary" onClick={onOpenTour}>
+          <img src={tourCastomIcons.tourBtn} style={iconSize} alt="иконка тура" />
+        </CustomBtn>
 
       </footer>
 
