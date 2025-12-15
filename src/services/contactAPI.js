c2bede3f99b5cf68ce2afcd7265ce1382ab458a2
Roannovna <RoAnna.97line@gmail.com>
2025-12-15 10:00:42 +0300

    feat: added a parameter that generates the same data set every time

--- src/services/contactAPI.js
+++ src/services/contactAPI.js
@@ -1,5 +1,5 @@
 export async function fetchContacts() {
-  const response = await fetch('https://randomuser.me/api/?results=200&nat=us');
+  const response = await fetch('https://randomuser.me/api/?results=200&nat=us&seed=portfolio');
   const data = await response.json();
   return data.results;
 }
